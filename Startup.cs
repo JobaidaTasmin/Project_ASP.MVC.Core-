9ac7235ef3f720e34f8525912a51fb2514130400
Jobaida Tasmin <106535311+JobaidaTasmin@users.noreply.github.com>
2022-11-11 20:48:37 +0600

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(DiagonsticTestManagement.Startup))]
+
+namespace DiagonsticTestManagement
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+
+            {
+
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+
+                LoginPath = new PathString("/Account/Login")
+
+            });
+        }
+    }
+}
